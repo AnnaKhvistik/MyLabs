bcb8f22a2e999865073182565554290497a85772
Анна Хвистик <anutahvistik@gmail.com>
2014-05-13 23:16:57 +0300

    11.1

---
+++ 11_task/11.1(1)/11.1(1)/1.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char str1[100], str2[100], result[100] = "";
+	fgets(str1, 100, stdin);
+	fgets(str2, 100, stdin);
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+
+
+	_asm
+	{
+			cld
+			lea esi, str1
+			xor ebx, ebx
+			xor edx, edx
+beg:
+		cmp ebx, len1
+			je end_
+			lea edi, str2
+			mov ecx, len2
+			inc ecx
+			mov al, str1[ebx]
+		repne scasb
+			jecxz not_in_str2
+			lea edi, result
+			mov ecx, edx
+			inc ecx
+
+			repne scasb
+			jecxz not_in_result
+
+not_in_str2:
+		inc ebx
+			jmp beg
+			;
+not_in_result:
+			mov result[edx], al
+				inc edx
+				;
+		inc ebx
+			jmp beg
+end_:
+		;
+	}
+
+
+
+
+	cout << result;
+}
